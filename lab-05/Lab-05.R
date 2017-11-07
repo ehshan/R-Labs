f6653a97c5895480eee122e12db92be11c343bfe
Ehshan Veerabangsa <ehshan@msn.com>
2017-11-07 20:39:15 +0000

    added p1 -> train/test split

--- lab-05/Lab-05.R
+++ lab-05/Lab-05.R
@@ -1,5 +1,5 @@
 # Clear enviroment 
-#rm(list = ls())
+rm(list = ls())
 
 # Load Data 
 library(ISLR)
@@ -8,5 +8,19 @@ summary(Auto)
 
 nrow(Auto)
 
-Auto
 
+# Part 1: Split data 
+
+# Define the sample size (50% of dataset)
+sample_size <- floor(0.5 * nrow(Auto))
+
+set.seed(1)
+
+# Split data 50/50
+#train = sample(1:nrow (Auto), nrow(Auto)/2)
+#train = sample(392, 196)
+train_index <- sample(seq_len(nrow(Auto)), size = sample_size)
+
+# Apply split to datset
+train <- Auto[train_index, ]
+test <- Auto[-train_index, ]
