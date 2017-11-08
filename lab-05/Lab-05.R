8a263172ecf9c021b4e527049bffb14a486e06d7
Ehshan Veerabangsa <ehshan@msn.com>
2017-11-08 19:19:18 +0000

    added p6 -> ploy of polynomial models

--- lab-05/Lab-05.R
+++ lab-05/Lab-05.R
@@ -1,5 +1,6 @@
+library("ggplot2")
 # Clear enviroment 
-rm(list = ls())
+#rm(list = ls())
 
 # Load Data 
 library(ISLR)
@@ -20,6 +21,7 @@ set.seed(1)
 #train = sample(1:nrow (Auto), nrow(Auto)/2)
 #train = sample(392, 196)
 train_index <- sample(seq_len(nrow(Auto)), size = sample_size)
+
 
 # Apply split to datset
 train <- Auto[train_index, ]
@@ -57,4 +59,49 @@ mean((test$mpg-predict(lm.fit3.train, test))^2)# has a smaller test error
 # THe improvement from the linear model (1st degree) to the quadratic model (2nd) is dramatic >20%.
 # The improvement from the quadratic to cubic (3r degree) is far less pronounced <1%.
 
+# Part 6: Plot polynomial models 
+
+
+rm(sample)
+
+# Dataframe 
+mse.degree <- data.frame( seed <- vector("integer", 100), degree <- vector("integer", 100), mse <- vector("double", 100))
+
+# column names 
+colnames(mse.degree) <- c("seed","degree", "mse")
+
+# counter variable for loops
+c <- 1
+
+# for each seed
+for (i in 1:10){
+  
+  set.seed(i)
+  train_index <- sample(seq_len(nrow(Auto)), size = sample_size)
+  
+  # Apply split to datset
+  train <- Auto[train_index, ]
+  test <- Auto[-train_index, ]
+  
+  # for each degree
+  for (j in 1:10) { 
+    
+    
+    # fit model to j degree
+    lm.fit.train <- lm(mpg~poly(horsepower, j), data = train)
+    
+    # Assign values to dataframe 
+    mse.degree[c, 2] <- j
+    mse.degree[c, 3] <- mean((test$mpg-predict(lm.fit.train, test))^2)
+    mse.degree[c, 1] <- i
+    
+    # increment
+    c <- c + 1
+
+  }
+  
+}
+
+# plot results 
+ggplot(mse.degree, aes(degree, mse)) + geom_line(aes(colour = seed))
 
