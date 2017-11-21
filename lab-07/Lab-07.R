e4f3034070149df40ec56c2fcef90cd2bbb369f2
Ehshan Veerabangsa <ehshan@msn.com>
2017-11-21 20:35:23 +0000

    initial commit - loaded dataset

---
+++ lab-07/Lab-07.R
@@ -0,0 +1,17 @@
+# Clear enviroment
+rm(list = ls())
+
+# Load data
+
+library(dplyr)
+
+rm(list = ls())
+
+titanic3 = read.csv("..\\Week08\\titanic3.cSV", header = TRUE)
+
+
+titanic3
+
+titanic3=select(titanic3, -name, -ticket, -boat, -body, -home.dest, -cabin) %>% mutate(embarked = factor(embarked),sex = factor(sex), pclass=factor(embarked))
+
+summary(titanic3)
