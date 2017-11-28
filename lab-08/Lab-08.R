6d5fd3d954bab7e42af91af9c02925af2afe4ec5
Ehshan Veerabangsa <ehshan@msn.com>
2017-11-28 20:59:59 +0000

    created training & test sets

--- lab-08/Lab-08.R
+++ lab-08/Lab-08.R
@@ -31,6 +31,16 @@ data <- data.frame(x = obs, y = as.factor(labels))
 plot(obs, col=ifelse( labels > 0, 1, 2))
 legend("topleft",c("Positive","Negative"),col=seq(2),pch=1,text.col=seq(2))
 
+# Split data into training & test sets
+
+set.seed(300)
+
+train_indices <- sample(200, 200 * 0.7)
+
+training_set <- data[train_indices, ]
+
+test_obs <- obs[-train_indices, ]
+test_lables <- labels[-train, ]
 
 
 # Part 1: Load SVM Library
