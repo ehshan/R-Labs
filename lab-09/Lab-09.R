405be6bf5e458596467fab736f7cd5b70955549c
Ehshan Veerabangsa <ehshan@msn.com>
2017-12-05 20:34:54 +0000

    Added part 4 - plot results

--- lab-09/Lab-09.R
+++ lab-09/Lab-09.R
@@ -15,20 +15,26 @@ dat = attitude[,c(3,4)]
 
 
 
+
 # Part 1: Plot data
 
 plot(dat, main = "% of favourable responses to Learning and Privilege", pch = 20, cex = 2)
 
 
-# Part 2: Perform k-means with 2 clusters, and 100 starting points
+# Part 2: Perform k-means with 2 clusters, and a single starting point
 
 set.seed(1)
 
-dat_k2 <- kmeans(dat,2, nstart = 100)
+dat_k2_1 <- kmeans(dat,2, nstart = 1)
 
-dat_k2$cluster
+dat_k2_1$cluster
 
 
 # Part 3: Calcualte the total within clusters sum of squares (SOS for all clusters)
 
-dat_k2$tot.withinss
+dat_k2_1$tot.withinss
+
+
+# Part 4: Plot the Clustering results
+
+plot(dat, col =(dat_k2_1$cluster +1) , main = "K-Means result with 2 clusters", pch = 20, cex = 2)
