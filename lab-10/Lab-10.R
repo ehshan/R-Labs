437a32c0022b4f05596693e3addf9e817e9207cb
Ehshan Veerabangsa <ehshan@msn.com>
2017-12-12 20:59:33 +0000

    Added part 2 - Analysis

--- lab-10/Lab-10.R
+++ lab-10/Lab-10.R
@@ -24,3 +24,18 @@ apply(USArrests, 2, mean)
 
 # calculate the variance
 apply(USArrests, 2, var)
+
+
+# Part 2: Analysis
+
+# If we look at the mean table, we can see that there is around 3 times more rapes than murders,
+# & around 8 times more assaults than rapes.
+
+# When we consider the variance table, we notice that, for the 3 crimes, assault has by the largest variance,
+# followed by rape. Generally the higher variance can be correlated higher mean crime rate. 
+
+# We Should also note that the figures for Urban population have no real realationship with the crime averages
+# as they are taken as a rate per 100000
+
+# As assault has by far the largest values for both tables, scaling will be required before PCA to avoid it
+# dominating it.
