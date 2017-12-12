507d235830b4d2965a856b7b31384b5dceafd7f7
Ehshan Veerabangsa <ehshan@msn.com>
2017-12-12 20:50:22 +0000

    Initial commit for lab 10 - exploring the data

---
+++ lab-10/Lab-10.R
@@ -0,0 +1,20 @@
+# Clear enviroment
+rm(list = ls())
+
+# Explore the datset
+
+?USArrests
+
+USArrests
+
+# List all rows headers
+states = row.names(USArrests)
+
+states
+
+
+# List all column headers
+names(USArrests)
+
+
+
